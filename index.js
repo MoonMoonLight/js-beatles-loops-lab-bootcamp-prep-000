fa2014c5b6b7e849611d1938bf82f12889b2caea
Priscila Moreno <priscila.moreno.wow@gmail.com>
2018-01-09 00:40:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
   for(let i= 0; i< musicians.length; i++){
-    array[i] = musicians[i] + ' plays ' + instruments[i]
+    array[i] = musicians[i] + ' plays ' + instruments[i];
   }
-  return array
+  return array;
+}
+
+function johnLennonFacts(array){
+  var x = 0;
+
+  var y = [];
+
+  while(x < array.length){
+    y[x] = array[x] + "!!!";
+    x++;
+  }
+  return y
 }
